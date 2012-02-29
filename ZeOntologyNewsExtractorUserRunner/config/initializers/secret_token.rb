d7f44fa1335b0661ac52b016fc69baecac424468
descl <descl@zouig.org>
2012-02-29 11:26:03 +0100

    first commit of ZONEUR

---
+++ ZeOntologyNewsExtractorUserRunner/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZeOntologyNewsExtractorUserRunner::Application.config.secret_token = '130e7232ab732803fc9005aeb314cb26f23175d58d699649bbb85e1d0b838c0cd2cf0d6c372d694014c33d8b883ca0024f5ee3996676e9f62189e57ee712cc88'
