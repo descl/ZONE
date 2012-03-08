5f1024c1c4d66024ec7f2557bb20bf0524128f36
descl <descl@zouig.org>
2012-03-08 22:12:57 +0100

    now we can say how many news we have in our database :p

--- ZeOntologyNewsExtractorUserRunner/app/controllers/home_controller.rb
+++ ZeOntologyNewsExtractorUserRunner/app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+    load 'lib/store.rb'
+    query = "SELECT DISTINCT ?concept WHERE {\n"
+    query += "?concept <http://purl.org/rss/1.0/title> ?title.
+    }"
+    endpoint = 'http://zouig.org:8081/sparql/'
+    puts query
+    store = FourStore::Store.new endpoint
+    @size = store.select(query).length
   end
 end
