4d3de03648c2543d85d48cd1cfb8e2fb8e43a794
descl <descl@zouig.org>
2012-03-08 23:11:49 +0100

    fix bug on news count

--- ZeOntologyNewsExtractorUserRunner/app/controllers/home_controller.rb
+++ ZeOntologyNewsExtractorUserRunner/app/controllers/home_controller.rb
@@ -1,12 +1,13 @@
 class HomeController < ApplicationController
   def index
     load 'lib/store.rb'
-    query = "SELECT DISTINCT ?concept WHERE {\n"
+    query = "SELECT DISTINCT (COUNT(?concept) as ?totalSize) WHERE {\n"
     query += "?concept <http://purl.org/rss/1.0/title> ?title.
     }"
     endpoint = 'http://zouig.org:8081/sparql/'
     puts query
     store = FourStore::Store.new endpoint
-    @size = store.select(query).length
+    puts store.select(query)
+    @size = store.select(query)
   end
 end
