f228cabd7cda0c75e2ff86cfecbbe9aafa405f3f
Christophe Desclaux <descl@zouig.org>
2012-11-29 15:07:15 +0100

    add InseeGeo to main annotation workflow

--- ZONE-extractor/ZONE-extractor-start/src/main/java/org/zoneproject/extractor/App.java
+++ ZONE-extractor/ZONE-extractor-start/src/main/java/org/zoneproject/extractor/App.java
@@ -15,12 +15,13 @@ public class App {
     public static void main(String[] args)  {
        Timer timer = new Timer();
 
-       String [] apps = new String[4];
+       String [] apps = new String[5];
        
        apps[0] = "org.zoneproject.extractor.rssreader.App";
        apps[1] = "org.zoneproject.extractor.plugin.extractarticlescontent.App";
        apps[2] = "org.zoneproject.extractor.plugin.opencalais.App";
        apps[3] = "org.zoneproject.extractor.plugin.wikimeta.App";
+       apps[4] = "org.zoneproject.extractor.plugin.inseegeo.App";
        for(String app : apps){
            timer.scheduleAtFixedRate( new ThreadApp(app),0, 30000); 
        
