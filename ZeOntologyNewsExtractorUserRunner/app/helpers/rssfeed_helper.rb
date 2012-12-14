784c462ac73534aba852b67ca590008953bdef59
Christophe Desclaux <descl@zouig.org>
2012-12-14 10:55:12 +0100

    [#41013697] fix bug inview for literals items comming from WikiMeta

--- ZeOntologyNewsExtractorUserRunner/app/helpers/rssfeed_helper.rb
+++ ZeOntologyNewsExtractorUserRunner/app/helpers/rssfeed_helper.rb
@@ -14,13 +14,17 @@ module RssfeedHelper
     if(item[1] == "null")
       item[1] = "/null"
     end
-    
     if(item[0].starts_with? @OPEN_CALAIS_URI)
       res= link_to item[0][@OPEN_CALAIS_URI.length,item[0].length], rssfeed_index_path(:old => @filter,:new => {:type => item[0], :value => item[1]}), :class => "btn btn-success"
       res+= link_to item[1], rssfeed_index_path(:old => @filter,:new => {:type => item[0], :value => item[1]}), :class => "btn btn-success"
     elsif(item[0].starts_with? @WIKI_META_URI)
       res=link_to item[0][@WIKI_META_URI.length,item[0].length], rssfeed_index_path(:old => @filter,:new => {:type => item[0], :value => item[1]}), :class => "btn btn-info"
-      res+=link_to item[1][item[1].rindex('/')+1, item[1].length], rssfeed_index_path(:old => @filter,:new => {:type => item[0], :value => item[1]}), :class => "btn btn-info"
+      if(item[1].rindex('/') == nil)
+        res+=link_to item[1], rssfeed_index_path(:old => @filter,:new => {:type => item[0], :value => item[1]}), :class => "btn btn-info"
+
+      else
+        res+=link_to item[1][item[1].rindex('/')+1, item[1].length], rssfeed_index_path(:old => @filter,:new => {:type => item[0], :value => item[1]}), :class => "btn btn-info"
+      end
     elsif(item[0].starts_with? @INSEE_GEO_URI)
       res=link_to item[0][@INSEE_GEO_URI.length,item[0].length], rssfeed_index_path(:old => @filter,:new => {:type => item[0], :value => item[1]}), :class => "btn btn-warning"
       res+=link_to item[1][item[1].rindex('/')+1, item[1].length], rssfeed_index_path(:old => @filter,:new => {:type => item[0], :value => item[1]}), :class => "btn btn-warning"
