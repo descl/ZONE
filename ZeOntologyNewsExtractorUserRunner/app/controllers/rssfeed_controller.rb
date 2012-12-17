cb526742f56f3a79aaf3d82da6a1cadf573c645b
Christophe Desclaux <descl@zouig.org>
2012-12-17 11:04:05 +0100

    [#41304507] show number of articles for filters

--- ZeOntologyNewsExtractorUserRunner/app/controllers/rssfeed_controller.rb
+++ ZeOntologyNewsExtractorUserRunner/app/controllers/rssfeed_controller.rb
@@ -1,5 +1,6 @@
 require 'sparql/client'
 class RssfeedController < ApplicationController
+  
   $endpoint = 'http://localhost:8890/sparql/'
   def getOne
     @itemURI = params[:element]
@@ -11,32 +12,7 @@ class RssfeedController < ApplicationController
   end
   
   def index
-    @filter = Array.new
-    extendQuery = ""
-    puts params
-    if(params.length > 2)
-      puts 'ya des choses'
-      #item = {"type"=> params[:type], "value"=>params[:value]}
-      puts params[:old]
-      if params[:old] != nil
-        params[:old].each do |cur|
-          @filter.push cur
-        end
-      end
-      
-      if params[:new] != nil
-        @filter.push params[:new]
-      end
-      
-      @filter.each do |cur|
-        if cur["value"].match(/^http\:\/\//)
-          #URI
-          extendQuery += "?concept <"+cur["type"]+"> <"+cur["value"]+">. \n"
-        else
-          extendQuery += "?concept <"+cur["type"]+"> '"+cur["value"]+"'. \n"
-        end
-      end
-    end
+    extendQuery = generateFilterRequest(params)
     
     @query = "SELECT ?concept ?relation ?result ?pubDateTime WHERE {\n"
     @query += extendQuery
@@ -45,10 +21,10 @@ class RssfeedController < ApplicationController
 ?concept ?relation ?result.
 } ORDER BY DESC(?pubDateTime) LIMIT 500"
 
-    puts @query
     store = SPARQL::Client.new($endpoint)
     @elements = store.query(@query)
 
+    @number = getNumberForFilter(extendQuery)
 # @result = Array.new
 # if @elements.length > 0
 # item = {"concept" => @elements[0]["concept"]}
@@ -86,5 +62,48 @@ class RssfeedController < ApplicationController
     #@result.each.delete_if{|x| x["http://purl.org/rss/1.0/link"] == nil}
     #@result.delete_if{|x| x["http://purl.org/rss/1.0/description"] == nil}
     
+  end
+  
+  
+  
+  
+  
+  
+  def getNumberForFilter(filter)
+    @query = "SELECT ?number COUNT(DISTINCT ?concept)  WHERE {\n"
+    @query += filter
+    @query += "?concept <http://purl.org/rss/1.0/title> ?title.
+} LIMIT 2"
+    store = SPARQL::Client.new($endpoint)
+    return store.query(@query)[0]["callret-1"]
+  end
+  
+  def generateFilterRequest(params)
+    @filter = Array.new
+    extendQuery = ""
+    puts params
+    if(params.length > 2)
+      #item = {"type"=> params[:type], "value"=>params[:value]}
+      puts params[:old]
+      if params[:old] != nil
+        params[:old].each do |cur|
+          @filter.push cur
+        end
+      end
+      
+      if params[:new] != nil
+        @filter.push params[:new]
+      end
+      
+      @filter.each do |cur|
+        if cur["value"].match(/^http\:\/\//)
+          #URI
+          extendQuery += "?concept <"+cur["type"]+"> <"+cur["value"]+">. \n"
+        else
+          extendQuery += "?concept <"+cur["type"]+"> '"+cur["value"]+"'. \n"
+        end
+      end
+    end
+    extendQuery
   end
 end
