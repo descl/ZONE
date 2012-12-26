10db2c322337277d9366c57b4573edcba1445bb6
Christophe Desclaux <descl@zouig.org>
2012-12-26 10:49:17 +0100

    [#41673069]add the new client project

---
+++ ZONE-watcher/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZONEWatcher::Application.config.secret_token = 'd27df92122a31845e7ab52278c371907e7623e9503e8e5b92be387ee76be6c1033d7111d5a41b05a920222d3b07c6af8c8d4a641fc0e68f0bfcbb76a69ab3b55'
