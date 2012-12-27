fc6fdf2acee8568aa830560df39f364d4e32e474
Christophe Desclaux <descl@zouig.org>
2012-12-27 11:34:28 +0100

    [#41673069] add the toogle accordion table javascript

--- ZONE-watcher/app/controllers/items_controller.rb
+++ ZONE-watcher/app/controllers/items_controller.rb
@@ -37,7 +37,9 @@ class ItemsController < ApplicationController
   # GET /items/1.json
   def show
     require 'cgi'
+    require 'digest'
     @uri = CGI.escape(params[:id])
+    @uriHash = Digest::SHA1.hexdigest(@uri)
     @item = Item.find(@uri)
     render  :layout => 'empty'
   end
