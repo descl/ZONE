4ee361d3a25a38ab750d158b91743133d373550c
Thibaut Comte <comte.thibaut@gmail.com>
2013-06-10 15:34:02 +0200

    changing all methods and images
    Methods, id, classes, images, variables are now in english for a better
    understanding.

--- ZONE-watcher/app/assets/javascripts/advanced_search.js
+++ ZONE-watcher/app/assets/javascripts/advanced_search.js
@@ -7,7 +7,7 @@ function changeDropdownKeyword(toto) {
 	$('#dropdownKeyword').html(toto);
 }
 
-function addFiltre() {
+function addFilter() {
 	if ($('#keyword').val() == "") {
 		return false
 	}
@@ -25,83 +25,73 @@ function addFiltre() {
 	}
 
 	var motcle = $('#keyword').val();
-	$('#tablesynonyme tbody > tr > td > label > input').each(function() {
+	$('#keywordTable tbody > tr > td > label > input').each(function() {
 		if ($(this).is(':checked')) {
 			motcle += ', ' + $(this).val();
 		}
 	});
 	var attr = $('#dropdownKeyword').html();
-	if ($('#tablefiltrage tr').length == 1)
+	if ($('#filteringTable tr').length == 1)
 		attr = "";
-	$('#tablefiltrage').append('<tr id="idrow' + idRowFiltrage + '" class="' + classtr + '"><td hidden>' + id + '</td><td class="tdkey">' + attr + '</td><td class="span3"><div class="tdval span9 breakword">' + motcle + '</div><button class="btn btn-danger pull-right"  onclick="removelinefiltrage(\'idrow' + idRowFiltrage + '\')"><i class="icon-remove"></i></button></td></tr>');
+	$('#filteringTable').append('<tr id="idrow' + idRowFiltrage + '" class="' + classtr + '"><td hidden>' + id + '</td><td class="tdkey">' + attr + '</td><td class="span3"><div class="tdval span9 breakword">' + motcle + '</div><button class="btn btn-danger pull-right"  onclick="removelinefiltrage(\'idrow' + idRowFiltrage + '\')"><i class="icon-remove"></i></button></td></tr>');
 	idRowFiltrage += 1;
 
-	rebootFiltrage();
-	//tritable();
-	affichfiltre();
+	rebootFiltering();
+	getFilter();
 }
 
-function rebootFiltrage() {
+function rebootFiltering() {
 	$('#keyword').val('');
 	$('#dropdownKeyword').text("OU");
 	$('#dropdownKeyword').val("OU");
 	unselect();
-	$('#tablesynonyme tbody > tr').each(function() {
+	$('#keywordTable tbody > tr').each(function() {
 		$(this).remove();
 	});
 }
 
-function tritable() {
-	var rowCount = $('#tablefiltrage >tbody >tr').length;
-	if (rowCount != 0) {
-		$("#tablefiltrage").tablesorter({
-			sortList : [[0, 0]]
-		});
-	}
-}
-
-function affichfiltre() {
-	$("#areafiltre").val('');
+function getFilter() {
+	$("#filteringArea").val('');
 	var i = 0;
-	$('#tablefiltrage tbody > tr').each(function() {
+	$('#filteringTable tbody > tr').each(function() {
 		if (i == 0)
-			$("#areafiltre").val($("#areafiltre").val() + $(this).find('div.tdval').html() + ' ');
+			$("#filteringArea").val($("#filteringArea").val() + $(this).find('div.tdval').html() + ' ');
 		else
-			$("#areafiltre").val($("#areafiltre").val() + $(this).find('td.tdkey').html() + ' ' + $(this).find('div.tdval').html() + ' ');
+			$("#filteringArea").val($("#filteringArea").val() + $(this).find('td.tdkey').html() + ' ' + $(this).find('div.tdval').html() + ' ');
 		i++;
 	});
 
 }
 
 function select() {
-	$('#tablesynonyme tbody > tr > td > label > input').each(function() {
+	$('#keywordTable tbody > tr > td > label > input').each(function() {
 		$(this).prop('checked', true);
 	});
 }
 
 function unselect() {
-	$('#tablesynonyme tbody > tr > td > label > input').each(function() {
+	$('#keywordTable tbody > tr > td > label > input').each(function() {
 		$(this).prop('checked', false);
 	});
 }
 
 function removelinefiltrage(line) {
 	$('#' + line).remove();
-	var usersTable = $("#tablefiltrage");
+	var usersTable = $("#filteringTable");
 	usersTable.trigger("update");
 
-	affichfiltre();
+	getFilter();
 }
 
-function MAJtablesynonyme() {
+function updateKeywordTable() {
 	if ($('#keyword').val() == "") {
-		$('#tablesynonyme tbody > tr').each(function() {
+		$('#keywordTable tbody > tr').each(function() {
 			$(this).remove();
 		});
 	}
 	$('#fakesynonyme tbody > tr').each(function() {
 		if ($('#keyword').val() == $(this).find('td.mot').html()) {
-			$('#tablesynonyme').append('<tr><td><label class="checkbox"><input type="checkbox" value="' + $(this).find('td.synonyme').html() + '">' + $(this).find('td.synonyme').html() + '</label></td></tr>');
+			$('#keywordTable').append('<tr><td><label class="checkbox"><input type="checkbox" value="' + $(this).find('td.synonyme').html() + '">' + $(this).find('td.synonyme').html() + '</label></td></tr>');
 		}
 	});
 }
@@ -109,11 +99,11 @@ function MAJtablesynonyme() {
 /** sources **/
 idRowSource = 0;
 
-function removelinesource(line) {
+function removeSourceLine(line) {
 	$('#' + line).remove();
 }
 
-function addRow(Form) {
+function addRowSourceTable(Form) {
 	if (Form.elements[0].value == "" && Form.elements[1].value == "") {
 		return false
 	}
@@ -122,31 +112,29 @@ function addRow(Form) {
 
 	var tdicone = "";
 	if (Form.name == "formTwitter")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/logotwitter.jpg" width="40" height="40";/></td>';
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundTwitter.jpg" width="40" height="40";/></td>';
 	else if (Form.name == "formGoogle")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/logog+.jpg" width="40" height="40";/></td>';
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundGoogle.jpg" width="40" height="40";/></td>';
 	else if (Form.name == "formRSS")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/logorss.jpg" width="40" height="40";/></td>';
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundRSS.jpg" width="40" height="40";/></td>';
 
 	var text = "";
 	var tdsource = "";
 	if (Form.elements[0].value != "" && Form.name != "formRSS") {
-		//text = coupeText(Form.elements[0].value);
 		text = Form.elements[0].value;
 		tdsource = '<td><i class="icon-user span1" style="padding-top:5px;"></i><div class="span8 breakword" contenteditable>' + text;
 		textefinal = "<tr class='user' id='idsource" + idRowSource + "'>";
 	} else if (Form.elements[1].value != "") {
-		//text = coupeText(Form.elements[1].value);
 		text = Form.elements[1].value;
 		tdsource = '<td><i class="icon-search span1" style="padding-top:5px;"></i> <div class="span8 breakword" contenteditable>' + text;
 		textefinal = "<tr class='search' id='idsource" + idRowSource + "'>";
 	}
 
-	tdaction = '</div><button class="btn pull-right btn-danger" onclick="removelinesource(\'idsource' + idRowSource + '\')"><i class="icon-remove icon-white" ></i></button></td>';
+	tdaction = '</div><button class="btn pull-right btn-danger" onclick="removeSourceLine(\'idsource' + idRowSource + '\')"><i class="icon-remove icon-white" ></i></button></td>';
 	idRowSource += 1;
 
 	textefinal = textefinal + tdicone + tdsource + tdaction + "/<tr>";
-	$('#tablesource').append(textefinal);
+	$('#sourceTable').append(textefinal);
 	rebootForm(Form);
 }
 
@@ -157,61 +145,43 @@ function rebootForm(Form) {
 	}
 }
 
-function modifLine() {
-	var nbline = $('#modalnbline').val();
-	var table = document.getElementById("tablesource");
-	var text = "";
-
-	if ($('#modalpseudo').val() != "") {
-		text = '<i class="icon-user"></i> ' + $('#modalpseudo').val();
-		table.rows[nbline].className = "user";
-	} else if ($('#modalrecherche').val() != "") {
-		text = '<i class="icon-search"></i> ' + $('#modalrecherche').val();
-		table.rows[nbline].className = "search";
-	} else {
-		return false;
-	}
-	var tdaction = table.rows[nbline].cells[1].innerHTML.split('<button');
-	table.rows[nbline].cells[1].innerHTML = text + "<button " + tdaction[1];
-}
-
-function addAll(Form) {
+function addAllSourceTable(Form) {
 	var textefinal = "";
 
 	var tdicone = "";
 	if (Form.name == "formTwitter")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/logotwitter.jpg" width="40" height="40";/></td>';
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundTwitter.jpg" width="40" height="40";/></td>';
 	else if (Form.name == "formGoogle")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/logog+.jpg" width="40" height="40";/></td>';
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundGoogle.jpg" width="40" height="40";/></td>';
 	else if (Form.name == "formRSS")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/logorss.jpg" width="40" height="40";/></td>';
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundRSS.jpg" width="40" height="40";/></td>';
 
 	var tdsource = "<td><i>Toutes les sources</i>";
 
 	textefinal = "<tr class='search' id='idsource" + idRowSource + "'>";
 
-	tdaction = '<button class="btn pull-right btn-danger"  onclick="removelinesource(\'idsource' + idRowSource + '\')"  ><i class="icon-remove icon-white"></i></button></td>';
+	tdaction = '<button class="btn pull-right btn-danger"  onclick="removeSourceLine(\'idsource' + idRowSource + '\')"  ><i class="icon-remove icon-white"></i></button></td>';
 	idRowSource += 1;
 
 	textefinal = textefinal + tdicone + tdsource + tdaction + "/<tr>";
-	$('#tablesource').append(textefinal);
+	$('#sourceTable').append(textefinal);
 	rebootForm(Form);
 }
 
-function rebootTableSource() {
-	$('#tablesource tbody > tr').each(function() {
+function rebootSourceTable() {
+	$('#sourceTable tbody > tr').each(function() {
 		$(this).remove();
 	});
 }
 
-function rebootTableFiltrage() {
-	$('#tablefiltrage tbody > tr').each(function() {
+function rebootFilteringTable() {
+	$('#filteringTable tbody > tr').each(function() {
 		$(this).remove();
 	});
 }
 
-function rebootTableSynonyme() {
-	$('#tablesynonyme tbody > tr').each(function() {
+function rebootKeywordTable() {
+	$('#keywordTable tbody > tr').each(function() {
 		$(this).remove();
 	});
 }
@@ -220,14 +190,14 @@ function rebootGeneralModal() {
 	rebootForm(formTwitter);
 	rebootForm(formRSS);
 	rebootForm(formGoogle);
-	rebootTableSource();
+	rebootSourceTable();
 	
-	rebootFiltrage();
-	rebootTableFiltrage();
-	rebootTableSynonyme();
+	rebootFiltering();
+	rebootFilteringTable();
+	rebootKeywordTable();
 	
-	affichfiltre();
+	getFilter();
 	
 	$('#Sources').addClass('active');
-	$('#Filtrage').removeClass('active');	
+	$('#Filtering').removeClass('active');	
 }
