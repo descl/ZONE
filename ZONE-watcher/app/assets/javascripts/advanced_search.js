a25e521ca8219690936403cacc4985872b92f5cf
Thibaut Comte <comte.thibaut@gmail.com>
2013-06-18 15:57:10 +0200

    Put all the text in .yml
    Commenting all the code
    
    Put all the text in the .yml file

--- ZONE-watcher/app/assets/javascripts/advanced_search.js
+++ ZONE-watcher/app/assets/javascripts/advanced_search.js
@@ -1,18 +1,22 @@
 /** filtrage**/
 
-idRowFiltrage = 0;
+//General ID for the filters that the user pick
+idRowFiltering = 0;
 
-function changeDropdownKeyword(toto) {
-	$('#dropdownKeyword').val(toto);
-	$('#dropdownKeyword').html(toto);
+//Change the dropdown selector for the filtering
+function changeDropdownKeyword(keyword) {
+	$('#dropdownKeyword').text(keyword);
+	$('#dropdownKeyword').val(keyword);
 }
 
+//Add a filter to the list of filter
 function addFilter() {
 	if ($('#keyword').val() == "") {
 		return false
 	}
 	var id = 1;
 	var classtr = "";
+	//Choose the class of the line of the table ( red for WITHOUT, blue for OR, green for AND)
 	if ($('#dropdownKeyword').val() == "ET") {
 		id = 1;
 		classtr = 'success';
@@ -24,22 +28,33 @@ function addFilter() {
 		classtr = 'error';
 	}
 
+	//Add the related word to the keyword
 	var motcle = $('#keyword').val();
 	$('#keywordTable tbody > tr > td > label > input').each(function() {
 		if ($(this).is(':checked')) {
 			motcle += ', ' + $(this).val();
 		}
 	});
+	
+	
 	var attr = $('#dropdownKeyword').val();
+	
+	//Change the dropdwonselector ( take away WITH and WITHOUT and put AND, OR , WITHOUT instead)
 	if ($('#filteringTable tr').length == 1)
 		$('.dropdownKeywordFiltering').html('<li onclick="changeDropdownKeyword(\'OU\')">OU</li><li onclick="changeDropdownKeyword(\'ET\')">ET</li><li onclick="changeDropdownKeyword(\'SANS\')">SANS</li>');
-	$('#filteringTable').append('<tr id="idrow' + idRowFiltrage + '" class="' + classtr + '"><td hidden>' + id + '</td><td class="tdkey">' + attr + '</td><td class="span3"><div class="tdval span9 breakword">' + motcle + '</div><button class="btn btn-danger pull-right"  onclick="removelinefiltrage(\'idrow' + idRowFiltrage + '\')"><i class="icon-remove"></i></button></td></tr>');
-	idRowFiltrage += 1;
+	
+	//Add the line to the table
+	$('#filteringTable').append('<tr id="idrow' + idRowFiltering + '" class="' + classtr + '"><td hidden>' + id + '</td><td class="tdkey">' + attr + '</td><td class="span3"><div class="tdval span9 breakword">' + motcle + '</div><button class="btn btn-danger pull-right"  onclick="removelinefiltrage(\'idrow' + idRowFiltering + '\')"><i class="icon-remove"></i></button></td></tr>');
+	idRowFiltering += 1;
 
+	//Reset the filtering
 	rebootFiltering();
+	
+	//Write the filter in the easy-box
 	getFilter();
 }
 
+//Reset the dropdown selector and the table of related word
 function rebootFiltering() {
 	$('#keyword').val('');
 	if ($('#filteringTable tr').length == 1) {
@@ -56,6 +71,7 @@ function rebootFiltering() {
 	});
 }
 
+//Write the filter in the easy-filter box
 function getFilter() {
 	$("#filteringArea").val('');
 	$('#filteringTable tbody > tr').each(function() {
@@ -64,23 +80,27 @@ function getFilter() {
 
 }
 
+//Select all the related words in the related table
 function select() {
 	$('#keywordTable tbody > tr > td > label > input').each(function() {
 		$(this).prop('checked', true);
 	});
 }
 
+//Unselect all the related words in the related table
 function unselect() {
 	$('#keywordTable tbody > tr > td > label > input').each(function() {
 		$(this).prop('checked', false);
 	});
 }
 
+//Remove one line of the filtering
 function removelinefiltrage(line) {
 	$('#' + line).remove();
 	var usersTable = $("#filteringTable");
 	usersTable.trigger("update");
 
+	//Set the filter in the easy-filter box
 	getFilter();
 
 	if ($('#filteringTable tr').length == 1)
@@ -89,6 +109,7 @@ function removelinefiltrage(line) {
 	rebootFiltering();
 }
 
+//Fill the related keyword table with the right keyword
 function updateKeywordTable() {
 	rebootKeywordTable();
 	$('#fakesynonyme tbody > tr').each(function() {
@@ -99,12 +120,16 @@ function updateKeywordTable() {
 }
 
 /** sources **/
+
+//General ID for the sources that the user pick
 idRowSource = 0;
 
+//Remove one line of the sources table
 function removeSourceLine(line) {
 	$('#' + line).remove();
 }
 
+//Add one line in the source table
 function addRowSourceTable(Form) {
 	if ($(Form + " .inputLogin").val() == "" && $(Form + " .inputSearch").val() == "") {
 		return false
@@ -112,6 +137,7 @@ function addRowSourceTable(Form) {
 
 	var textefinal = "";
 
+	//Pick the icon corresponding to the source ( twitter, g+, rss,...)
 	var tdicone = "";
 	if (Form == "#formTwitter")
 		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundTwitter.jpg" width="40" height="40";/><input class="valSource" type="hidden" value="twitter"></td>';
@@ -122,6 +148,8 @@ function addRowSourceTable(Form) {
 
 	var text = "";
 	var tdsource = "";
+	
+	//Pick the icon corresponding to the type of the source ( search or people)
 	if ($(Form + " .inputLogin").val() != "" && Form != "#formRSS") {
 		text = $(Form + " .inputLogin").val();
 		tdsource = '<td><i class="icon-user span1" style="padding-top:5px;"></i><div class="span8 breakword" contenteditable>' + text;
@@ -132,23 +160,32 @@ function addRowSourceTable(Form) {
 		textefinal = "<tr class='search' id='idsource" + idRowSource + "'>";
 	}
 
+	//Define the button to delete a line in the table
 	tdaction = '</div><button class="btn pull-right btn-danger" onclick="removeSourceLine(\'idsource' + idRowSource + '\')"><i class="icon-remove icon-white" ></i></button></td>';
 	idRowSource += 1;
 
 	textefinal = textefinal + tdicone + tdsource + tdaction + "/<tr>";
+	
+	//add the line
 	$('#sourceTable').append(textefinal);
+	
+	//Reset the input of the source
 	rebootForm(Form);
 }
 
+//Reset the input of the source
 function rebootForm(Form) {
 	$(Form + " .inputLogin").val('');
 	$(Form + " .inputSearch").val('');
 }
 
+//Add one line with all sources in the source table
 function addAllSourceTable(Form) {
 	var textefinal = "";
 
 	var tdicone = "";
+	
+	//Pick the icon corresponding to the source ( twitter, g+, rss,...)
 	if (Form == "formTwitter")
 		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundTwitter.jpg" width="40" height="40";/><input class="valSource" type="hidden" value="twitter"></td>';
 	else if (Form == "formGoogle")
@@ -156,40 +193,50 @@ function addAllSourceTable(Form) {
 	else if (Form == "formRSS")
 		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundRSS.jpg" width="40" height="40";/><input class="valSource" type="hidden" value="rss"></td>';
 
-	var tdsource = "<td><i><div class='breakword'>Toutes les sources</div></i>";
+	var tdsource = "<td><i><div class='breakword' style='float:left'>Toutes les sources</div></i>";
 
 	textefinal = "<tr class='search' id='idsource" + idRowSource + "'>";
 
+	//Define the button to delete a line in the table
 	tdaction = '<button class="btn pull-right btn-danger"  onclick="removeSourceLine(\'idsource' + idRowSource + '\')"  ><i class="icon-remove icon-white"></i></button></td>';
 	idRowSource += 1;
 
 	textefinal = textefinal + tdicone + tdsource + tdaction + "/<tr>";
+	
+	//add the line
 	$('#sourceTable').append(textefinal);
+	
+	//Reset the input of the source
 	rebootForm(Form);
 }
 
+//Reset the source table, Delete all lines
 function rebootSourceTable() {
 	$('#sourceTable tbody > tr').each(function() {
 		$(this).remove();
 	});
 }
 
+//Reset the filtering table, Delete all lines
 function rebootFilteringTable() {
 	$('#filteringTable tbody > tr').each(function() {
 		$(this).remove();
 	});
 }
 
+//Reset the related keyword table, Delete all lines
 function rebootKeywordTable() {
 	$('#keywordTable tbody > tr').each(function() {
 		$(this).remove();
 	});
 }
 
+//Reset the semantic search.
 function rebootGeneralModal() {
 	rebootForm('#formTwitter');
 	rebootForm('#formRSS');
 	rebootForm('#formGoogle');
+	
 	rebootSourceTable();
 
 	rebootFiltering();
@@ -211,24 +258,32 @@ function rebootGeneralModal() {
 
 }
 
+//Change the button that switch tab ( put everything ready to go to next tab)
 function setButtonNextTab() {
-	$('#btnTabSource').hide();
-	$('#btnTabFiltrage').show();
+	$('#btnTabPrevious').hide();
+	$('#btnTabNext').show();
 	$('#btnGoReador').hide();
-	$('#myModalLabel').text('Recherche sémantique : Sources');
+	$('.titleSource').removeClass('hidden');
+	$('.titleFiltering').addClass('hidden');
 }
 
+//Change the button that switch tab ( put everything ready to go to previous tab)
 function setButtonPreviousTab() {
-	$('#btnTabFiltrage').hide();
+	$('#btnTabNext').hide();
 	$('#btnGoReador').show();
-	$('#btnTabSource').show();
-	$('#myModalLabel').text('Recherche sémantique : Filtres');
+	$('#btnTabPrevious').show();
+	$('.titleSource').addClass('hidden');
+	$('.titleFiltering').removeClass('hidden');
 }
 
+//Prepare the input with all data
 function movingData() {
+	
 	var arraySourceAdded = new Array();
 	var arrayFilteringAdded = new Array();
 	var littleArray = new Array();
+	
+	//Prepare the array that will contain the sources data
 	$('#sourceTable tbody > tr ').each(function() {
 		if ($(this).find('input.valSource').val()) {
 			littleArray.push($(this).find('input.valSource').val());
@@ -240,6 +295,7 @@ function movingData() {
 		}
 	});
 
+	//Prepare the array that will contain the filtering data
 	$('#filteringTable tbody > tr ').each(function() {
 		if ($(this).find('td.tdkey').html()) {
 			littleArray.push($(this).find('td.tdkey').html());
@@ -250,6 +306,7 @@ function movingData() {
 			littleArray = new Array();
 		}
 	});
-	$('#movedData').html($('#movedData').html() + "<input name='arraySource' type='hidden' value='" + arraySourceAdded + "'>" + "<input name='arrayFiltering' type='hidden' value='" + arrayFilteringAdded + "'>");
-	return false;
+	
+	//create the input with the data
+	$('#movedData').html($('#movedData').html() + "<input name='arraySource' type='hidden' value='" + arraySourceAdded + "'>" + "<input name='arrayFiltering[]' type='hidden' value='" + arrayFilteringAdded + "'>");
 }
