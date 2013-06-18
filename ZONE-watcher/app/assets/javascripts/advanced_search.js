39964e005b655e3b875d620102b0f8a8d039229e
Thibaut Comte <comte.thibaut@gmail.com>
2013-06-18 14:15:16 +0200

    Data are now sent to the database for treatment
    Sources and filters can be received by the database using this format :
    Array for sources :
    name : arraySource
    format : [{twitter,google, rss},{ONE data},{twitter,google, rss},{ONE
    data},...]
    
    Array for filters :
    name : arrayFiltering
    format : [{AVEC,OU,ET,SANS},{ONE data, ONE data, ONE
    data,...},{AVEC,OU,ET,SANS},{ONE data, ONE data, ONE data,...}, ...]
    
    Added 4 images for future sources

--- ZONE-watcher/app/assets/javascripts/advanced_search.js
+++ ZONE-watcher/app/assets/javascripts/advanced_search.js
@@ -16,7 +16,7 @@ function addFilter() {
 	if ($('#dropdownKeyword').val() == "ET") {
 		id = 1;
 		classtr = 'success';
-	} else if ($('#dropdownKeyword').val() == "OU") {
+	} else if ($('#dropdownKeyword').val() == "OU" || $('#dropdownKeyword').val() == "AVEC") {
 		id = 0;
 		classtr = 'info';
 	} else if ($('#dropdownKeyword').val() == "SANS") {
@@ -30,9 +30,9 @@ function addFilter() {
 			motcle += ', ' + $(this).val();
 		}
 	});
-	var attr = $('#dropdownKeyword').html();
+	var attr = $('#dropdownKeyword').val();
 	if ($('#filteringTable tr').length == 1)
-		attr = "";
+		$('.dropdownKeywordFiltering').html('<li onclick="changeDropdownKeyword(\'OU\')">OU</li><li onclick="changeDropdownKeyword(\'ET\')">ET</li><li onclick="changeDropdownKeyword(\'SANS\')">SANS</li>');
 	$('#filteringTable').append('<tr id="idrow' + idRowFiltrage + '" class="' + classtr + '"><td hidden>' + id + '</td><td class="tdkey">' + attr + '</td><td class="span3"><div class="tdval span9 breakword">' + motcle + '</div><button class="btn btn-danger pull-right"  onclick="removelinefiltrage(\'idrow' + idRowFiltrage + '\')"><i class="icon-remove"></i></button></td></tr>');
 	idRowFiltrage += 1;
 
@@ -42,8 +42,14 @@ function addFilter() {
 
 function rebootFiltering() {
 	$('#keyword').val('');
-	$('#dropdownKeyword').text("OU");
-	$('#dropdownKeyword').val("OU");
+	if ($('#filteringTable tr').length == 1) {
+		$('#dropdownKeyword').text("AVEC");
+		$('#dropdownKeyword').val("AVEC");
+	} else {
+		$('#dropdownKeyword').text("OU");
+		$('#dropdownKeyword').val("OU");
+	}
+
 	unselect();
 	$('#keywordTable tbody > tr').each(function() {
 		$(this).remove();
@@ -52,13 +58,8 @@ function rebootFiltering() {
 
 function getFilter() {
 	$("#filteringArea").val('');
-	var i = 0;
 	$('#filteringTable tbody > tr').each(function() {
-		if (i == 0)
-			$("#filteringArea").val($("#filteringArea").val() + $(this).find('div.tdval').html() + ' ');
-		else
-			$("#filteringArea").val($("#filteringArea").val() + $(this).find('td.tdkey').html() + ' ' + $(this).find('div.tdval').html() + ' ');
-		i++;
+		$("#filteringArea").val($("#filteringArea").val() + $(this).find('td.tdkey').html() + ' ' + $(this).find('div.tdval').html() + ' ');
 	});
 
 }
@@ -81,6 +82,11 @@ function removelinefiltrage(line) {
 	usersTable.trigger("update");
 
 	getFilter();
+
+	if ($('#filteringTable tr').length == 1)
+		$('.dropdownKeywordFiltering').html('<li onclick="changeDropdownKeyword(\'AVEC\')">AVEC</li><li onclick="changeDropdownKeyword(\'SANS\')">SANS</li>');
+
+	rebootFiltering();
 }
 
 function updateKeywordTable() {
@@ -100,28 +106,28 @@ function removeSourceLine(line) {
 }
 
 function addRowSourceTable(Form) {
-	if (Form.elements[0].value == "" && Form.elements[1].value == "") {
+	if ($(Form + " .inputLogin").val() == "" && $(Form + " .inputSearch").val() == "") {
 		return false
 	}
 
 	var textefinal = "";
 
 	var tdicone = "";
-	if (Form.name == "formTwitter")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundTwitter.jpg" width="40" height="40";/></td>';
-	else if (Form.name == "formGoogle")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundGoogle.jpg" width="40" height="40";/></td>';
-	else if (Form.name == "formRSS")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundRSS.jpg" width="40" height="40";/></td>';
+	if (Form == "#formTwitter")
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundTwitter.jpg" width="40" height="40";/><input class="valSource" type="hidden" value="twitter"></td>';
+	else if (Form == "#formGoogle")
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundGoogle.jpg" width="40" height="40";/><input class="valSource" type="hidden" value="google"></td>';
+	else if (Form == "#formRSS")
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundRSS.jpg" width="40" height="40";/><input class="valSource" type="hidden" value="rss"></td>';
 
 	var text = "";
 	var tdsource = "";
-	if (Form.elements[0].value != "" && Form.name != "formRSS") {
-		text = Form.elements[0].value;
+	if ($(Form + " .inputLogin").val() != "" && Form != "#formRSS") {
+		text = $(Form + " .inputLogin").val();
 		tdsource = '<td><i class="icon-user span1" style="padding-top:5px;"></i><div class="span8 breakword" contenteditable>' + text;
 		textefinal = "<tr class='user' id='idsource" + idRowSource + "'>";
-	} else if (Form.elements[1].value != "") {
-		text = Form.elements[1].value;
+	} else if ($(Form + " .inputSearch").val() != "") {
+		text = $(Form + " .inputSearch").val();
 		tdsource = '<td><i class="icon-search span1" style="padding-top:5px;"></i> <div class="span8 breakword" contenteditable>' + text;
 		textefinal = "<tr class='search' id='idsource" + idRowSource + "'>";
 	}
@@ -135,24 +141,22 @@ function addRowSourceTable(Form) {
 }
 
 function rebootForm(Form) {
-	var key = 0;
-	for ( key = 0; key < Form.elements.length; key++) {
-		Form.elements[key].value = "";
-	}
+	$(Form + " .inputLogin").val('');
+	$(Form + " .inputSearch").val('');
 }
 
 function addAllSourceTable(Form) {
 	var textefinal = "";
 
 	var tdicone = "";
-	if (Form.name == "formTwitter")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundTwitter.jpg" width="40" height="40";/></td>';
-	else if (Form.name == "formGoogle")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundGoogle.jpg" width="40" height="40";/></td>';
-	else if (Form.name == "formRSS")
-		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundRSS.jpg" width="40" height="40";/></td>';
+	if (Form == "formTwitter")
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundTwitter.jpg" width="40" height="40";/><input class="valSource" type="hidden" value="twitter"></td>';
+	else if (Form == "formGoogle")
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundGoogle.jpg" width="40" height="40";/><input class="valSource" type="hidden" value="google"></td>';
+	else if (Form == "formRSS")
+		tdicone = '<td><img class="littleCircleImage sortable" src="/assets/foregroundRSS.jpg" width="40" height="40";/><input class="valSource" type="hidden" value="rss"></td>';
 
-	var tdsource = "<td><i>Toutes les sources</i>";
+	var tdsource = "<td><i><div class='breakword'>Toutes les sources</div></i>";
 
 	textefinal = "<tr class='search' id='idsource" + idRowSource + "'>";
 
@@ -183,33 +187,69 @@ function rebootKeywordTable() {
 }
 
 function rebootGeneralModal() {
-	rebootForm(formTwitter);
-	rebootForm(formRSS);
-	rebootForm(formGoogle);
+	rebootForm('#formTwitter');
+	rebootForm('#formRSS');
+	rebootForm('#formGoogle');
 	rebootSourceTable();
-	
+
 	rebootFiltering();
 	rebootFilteringTable();
 	rebootKeywordTable();
-	
+
 	getFilter();
-	
+
 	$('#Sources').addClass('active');
 	$('#Filtering').removeClass('active');
-	
+
 	setButtonNextTab();
+	
+	if ($('#filteringTable tr').length == 1){
+		$('.dropdownKeywordFiltering').html('<li onclick="changeDropdownKeyword(\'AVEC\')">AVEC</li><li onclick="changeDropdownKeyword(\'SANS\')">SANS</li>');
+		$('#dropdownKeyword').text("AVEC");
+		$('#dropdownKeyword').val("AVEC");
+	}
+
 }
 
-function setButtonNextTab(){
+function setButtonNextTab() {
 	$('#btnTabSource').hide();
 	$('#btnTabFiltrage').show();
 	$('#btnGoReador').hide();
 	$('#myModalLabel').text('Recherche sémantique : Sources');
 }
 
-function setButtonPreviousTab(){
+function setButtonPreviousTab() {
 	$('#btnTabFiltrage').hide();
 	$('#btnGoReador').show();
 	$('#btnTabSource').show();
 	$('#myModalLabel').text('Recherche sémantique : Filtres');
+}
+
+function movingData() {
+	var arraySourceAdded = new Array();
+	var arrayFilteringAdded = new Array();
+	var littleArray = new Array();
+	$('#sourceTable tbody > tr ').each(function() {
+		if ($(this).find('input.valSource').val()) {
+			littleArray.push($(this).find('input.valSource').val());
+			if ($(this).find('div.breakword').html() != null) {
+				littleArray.push($(this).find('div.breakword').html());
+			}
+			arraySourceAdded.push(littleArray);
+			littleArray = new Array();
+		}
+	});
+
+	$('#filteringTable tbody > tr ').each(function() {
+		if ($(this).find('td.tdkey').html()) {
+			littleArray.push($(this).find('td.tdkey').html());
+			if ($(this).find('div.breakword').html() != null) {
+				littleArray.push($(this).find('div.breakword').html());
+			}
+			arrayFilteringAdded.push(littleArray);
+			littleArray = new Array();
+		}
+	});
+	$('#movedData').html($('#movedData').html() + "<input name='arraySource' type='hidden' value='" + arraySourceAdded + "'>" + "<input name='arrayFiltering' type='hidden' value='" + arrayFilteringAdded + "'>");
+	return false;
 }
