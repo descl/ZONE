7efdb794c9d893cbff47e9f806d70e01eae97c96
Thibaut Comte <comte.thibaut@gmail.com>
2013-06-26 17:01:12 +0200

    Change items tag from button type to label type
    Remove the blank in front of the first tag

--- ZONE-watcher/app/helpers/items_helper.rb
+++ ZONE-watcher/app/helpers/items_helper.rb
@@ -13,25 +13,25 @@ module ItemsHelper
     end
     itemURI = items_path(:old => @filters,:new => filter)
     if(filter.prop.starts_with? @OPEN_CALAIS_URI)
-      res= link_to filter.prop[@OPEN_CALAIS_URI.length,filter.prop.length], itemURI, :class => "btn btn-success"
-      res+= link_to filter.value, itemURI, :class => "btn btn-success"
+      res= link_to filter.prop[@OPEN_CALAIS_URI.length,filter.prop.length], itemURI, :class => "label label-success"
+      res+= link_to filter.value, itemURI, :class => "label label-success"
     elsif(filter.prop.starts_with? @WIKI_META_URI)
-      res=link_to filter.prop[@WIKI_META_URI.length,filter.prop.length], itemURI, :class => "btn btn-info"
+      res=link_to filter.prop[@WIKI_META_URI.length,filter.prop.length], itemURI, :class => "label label-info"
       if(filter.value.rindex('/') == nil)
-        res+=link_to filter.value, itemURI, :class => "btn btn-info"
+        res+=link_to filter.value, itemURI, :class => "label label-info"
 
       else
-        res+=link_to filter.value[filter.value.rindex('/')+1, filter.value.length], itemURI, :class => "btn btn-info"
+        res+=link_to filter.value[filter.value.rindex('/')+1, filter.value.length], itemURI, :class => "label label-info"
       end
     elsif(filter.prop.starts_with? @INSEE_GEO_URI)
-      res=link_to filter.prop[@INSEE_GEO_URI.length,filter.prop.length], itemURI, :class => "btn btn-warning"
-      res+=link_to filter.value[filter.value.rindex('/')+1, filter.value.length], itemURI, :class => "btn btn-warning"
+      res=link_to filter.prop[@INSEE_GEO_URI.length,filter.prop.length], itemURI, :class => "label label-warning"
+      res+=link_to filter.value[filter.value.rindex('/')+1, filter.value.length], itemURI, :class => "label label-warning"
     elsif(filter.prop.start_with? @RSS_URI+"source")
-      res=link_to filter.prop[@RSS_URI.length,filter.prop.length], itemURI, :class => "btn btn-primary"
-      res+=link_to filter.value, itemURI, :class => "btn btn-primary"
+      res=link_to filter.prop[@RSS_URI.length,filter.prop.length], itemURI, :class => "label"
+      res+=link_to filter.value, itemURI, :class => "label"
     elsif(filter.prop.start_with? @SVM_PLUGIN_URI)
-      res=link_to "category", itemURI, :class => "btn btn-danger"
-      res+=link_to filter.value, itemURI, :class => "btn btn-danger"
+      res=link_to "category", itemURI, :class => "label label-important"
+      res+=link_to filter.value, itemURI, :class => "label label-important"
     elsif(filter.prop.start_with? @TWITTER_HASHTAG_PLUGIN_URI)
       res=link_to "#"+filter.value, itemURI, :class => "btn-auth  btn-twitter"
     elsif(filter.prop.start_with? @TWITTER_MENTIONED_PLUGIN_URI)
