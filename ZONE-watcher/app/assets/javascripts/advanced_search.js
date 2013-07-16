13592724318e7d8b1fd9ca5153c8a4f3ebf855b1
Thibaut Comte <comte.thibaut@gmail.com>
2013-07-16 09:27:23 +0200

    Corrected a bug in the transition

--- ZONE-watcher/app/assets/javascripts/advanced_search.js
+++ ZONE-watcher/app/assets/javascripts/advanced_search.js
@@ -506,6 +506,8 @@ function switchTab() {
 	$('#sources').fadeOut('swing', function() {
 		$('#filtering').fadeIn();
 	});
+	
+	$(".form").hide();
 }
 
 //Allow to reverse switch tab in the new semantic search
