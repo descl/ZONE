5115e9d3e3a218a9da5d425f11273b0b187997a6
Christophe Desclaux <descl@zouig.org>
2013-07-23 16:03:20 +0200

    [#53880527]clean code and move search tool to controller searches

--- ZONE-watcher/app/assets/javascripts/items.js.coffee
+++ ZONE-watcher/app/assets/javascripts/items.js.coffee
@@ -1,6 +1,5 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+#= require items_show
+#= require reminder_panel
 
 $(document).ready ->
   $.get gon.uriForItemsNumber, (data) ->
@@ -20,3 +19,57 @@ $(document).ready ->
         alert(xhr.status)
         alert(xhr.statusText)
         alert(thrownError)
+
+$(document).ready ->
+
+  #Hide the favorite bar by default
+  $(".row-favorite").hide()
+  $(".row-list").hide()
+
+  $(".hideTag").hide();
+  $(".btn-toolbar").hide();
+
+  #action to show the tag on click
+  $(".showTag").on "click", ->
+    $(this).parent().next(".btn-toolbar").slideDown()
+    $(this).hide()
+    $(this).next(".hideTag").show()
+
+
+  #Action to hide the tag on click
+  $(".hideTag").on "click", ->
+    $(this).parent().next(".btn-toolbar").slideUp()
+    $(this).hide()
+    $(this).prev(".showTag").show()
+
+
+  #Generation of the popover of the tag
+  btnOptionnal = ""
+  btnMust = ""
+  btnBan = ""
+  $(".label-tag").each ->
+    btnOptionnal = "<button type='button' class='btn btn-info span12 btnTag' onclick='addTag(\"opt\",\"" + $(this).html() + "\");closePop()'>" + $("#titleOr").html() + "</button><br>"
+    btnMust = "<button type='button' class='btn btn-success span12 btnTag' onclick='addTag(\"must\",\"" + $(this).html() + "\");closePop()'>" + $("#titleAnd").html() + "</button><br>"
+    btnBan = "<button type='button' class='btn btn-danger span12 btnTag' onclick='addTag(\"no\",\"" + $(this).html() + "\");closePop()'>" + $("#titleWithout").html() + "</button>"
+    $(this).popover
+      title: $("#titlePopover").html()
+      content: "<div class='row-fluid'><div class='span12'>" + btnOptionnal + "</div></div><div class='row-fluid'><div class='span12'>" + btnMust + "</div></div><div class='row-fluid'><div class='span12'>" + btnBan + "</div></div>"
+      placement: "bottom"
+
+
+
+  #Disable the default action onclick on the tag
+  $(".label-tag").on "click", ->
+    false
+
+  $(".showFavorite").hover (->
+    $(this).next(".row-favorite").fadeIn()
+    $(this).hide()
+  ), ->
+    $(this).hide()
+
+  $(".row-favorite").hover (->
+    $(this).prev(".showFavorite").hide()
+  ), ->
+    $(this).hide()
+    $(this).prev(".showFavorite").fadeIn()
---
+++ ZONE-watcher/app/assets/javascripts/items_show.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+    //Tweeter function
+    ! function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "https://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }(document, "script", "twitter-wjs");
+
+    //Google+ function
+    window.___gcfg = {
+        lang : 'fr'
+    };
+
+    //Googlefunction
+    (function() {
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(po, s);
+    })();
+
+    //Facebook function
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id))
+            return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+});
+
+
+//Add the tag to the summary panel
+function addTag(type, value) {
+    if (type == 'opt')
+        $(".well-info").append('<span class="label label-info">' + value + ' <i class="icon-remove" onclick="$(this).closest(&quot;span&quot;).remove();"></i></span> ');
+    else if (type == 'must')
+        $(".well-success").append('<span class="label label-success">' + value + ' <i class="icon-remove" onclick="$(this).closest(&quot;span&quot;).remove();"></i></span> ');
+    else if (type == 'no')
+        $(".well-danger").append('<span class="label label-danger">' + value + ' <i class="icon-remove" onclick="$(this).closest(&quot;span&quot;).remove();"></i></span> ');
+    return false;
+};
+
+//Close all the popover after clicking on a selction
+function closePop() {
+    $(".label-tag").popover('hide');
+    $('#openReminder').popover('show');
+    setTimeout(function() {
+        $('#openReminder').popover('hide');
+    }, 2000);
+    return false;
+}
+
+//Function that change the disposition of the items, used in /items
+function changeItemFormat(type) {
+    if (type == 'card') {
+        $("#btnCard").addClass('active');
+        $("#btnList").removeClass('active');
+
+        $(".item-bloc:even").addClass('span6 pull-left');
+        $(".item-bloc:even").addClass('clear-left');
+        $(".item-bloc:odd").addClass('span6 pull-right');
+        $(".item-bloc:odd").addClass('clear-right');
+
+        $(".row-favorite").hide();
+        $(".showFavorite").hide();
+        $(".row-list").show();
+    } else {
+        $("#btnList").addClass('active');
+        $("#btnCard").removeClass('active');
+
+        $('.item-bloc').removeClass('span6');
+        $('.item-bloc').removeClass('pull-left');
+        $('.item-bloc').removeClass('pull-right');
+        $('.item-bloc').removeClass('clear-right');
+        $('.item-bloc').removeClass('clear-left');
+
+        $(".row-list").hide();
+        $(".showFavorite").show();
+    }
+
+}
