ccc2994b09545bbf2f3dc346b539aa9dbda229a5
Christophe Desclaux <descl@zouig.org>
2013-07-25 12:06:54 +0200

    [#54015359] try to remove cache to make working the completion

--- ZONE-watcher/app/controllers/linked_words_controller.rb
+++ ZONE-watcher/app/controllers/linked_words_controller.rb
@@ -1,5 +1,5 @@
 class LinkedWordsController < ApplicationController
-  caches_page :listWords, :autoComplete
+  #caches_page :listWords, :autoComplete, :expires_in => 5.minutes
 
   # GET /linked_words
   # GET /linked_words/$entity.json
@@ -21,11 +21,12 @@ class LinkedWordsController < ApplicationController
   end
 
   def autoComplete
-    @result = LinkedWord.complete(params[:desc])
-
-    respond_to do |format|
-      format.json {
-        render json: @result }
-    end
-  end
+    #cache(params[:desc], :expires_in => 10.minute) do
+      @result = LinkedWord.complete(params[:desc])
+      respond_to do |format|
+        format.json {
+          render json: @result }
+        end
+      end
+    #end
 end
