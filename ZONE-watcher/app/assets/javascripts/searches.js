cbb6dcce490c04ad76e4bd2b0634b7feb6b2226c
Thibaut Comte <comte.thibaut@gmail.com>
2013-08-07 11:19:24 +0200

    Implements the opml import

--- ZONE-watcher/app/assets/javascripts/searches.js
+++ ZONE-watcher/app/assets/javascripts/searches.js
@@ -238,29 +238,6 @@ function reverseSwitchTab() {
     });
 }
 
-//Extrait filename d'un input file
-function extractFilename(path) {
-  if (path.substr(0, 12) == "C:\\fakepath\\")
-    return path.substr(12); // modern browser
-  var x;
-  x = path.lastIndexOf('/');
-  if (x >= 0) // Unix-based path
-    return path.substr(x+1);
-  x = path.lastIndexOf('\\');
-  if (x >= 0) // Windows-based path
-    return path.substr(x+1);
-  return path; // just the filename
-}
-
-//Update the filename in the textfield
-function updateFilename(path) {
-   var name = extractFilename(path);
-   	if(name != "")
-		$('#fileTextName').html(name);
-	else
-		$('#fileTextName').html($('#fileTextName').attr('data-initial'));
-}
-
 //show or hide the import rss panel
 function showImportRss(){
 	if ( $(".importRss").is(":visible")){
