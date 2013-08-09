925066276730146a22c3634fe510c7d2cc77abe2
Christophe Desclaux <descl@zouig.org>
2013-08-09 16:45:57 +0200

    Merge pull request #45 from tcomte

--- ZONE-watcher/app/controllers/search_filters_controller.rb
+++ ZONE-watcher/app/controllers/search_filters_controller.rb
@@ -5,8 +5,7 @@ class SearchFiltersController < ApplicationController
   # GET /searches/1
   # GET /searches/1.json
   def index
-    @search_filter = SearchFilter.new(:uri => params[:uri])
-    #@infos = @search_filter.getInfos
+    @infos = @search_filter.getInfos
     @infos = {:thumbnail => ""}
 
     render  :layout => 'empty'
