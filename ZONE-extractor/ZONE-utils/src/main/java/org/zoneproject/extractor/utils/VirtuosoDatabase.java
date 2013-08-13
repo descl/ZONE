05b0ab3bf8372308d987e97c6ed6d3a9a80231a5
Christophe Desclaux <descl@zouig.org>
2013-08-13 15:26:43 +0200

    [extractor] add timeout for case in which the triplestore take too much time to respond

--- ZONE-extractor/ZONE-utils/src/main/java/org/zoneproject/extractor/utils/VirtuosoDatabase.java
+++ ZONE-extractor/ZONE-utils/src/main/java/org/zoneproject/extractor/utils/VirtuosoDatabase.java
@@ -68,9 +68,22 @@ public abstract class VirtuosoDatabase {
         }
         return st;
     }
+    
     public static Model getStore(String graph){
-        return VirtModel.openDatabaseModel(graph, VIRTUOSO_SERVER, VIRTUOSO_USER, VIRTUOSO_PASS);
+        return getStore(graph,0);
     }
+    
+    private static Model getStore(String graph,int testNumber){
+        try{
+            return VirtModel.openDatabaseModel(graph, VIRTUOSO_SERVER, VIRTUOSO_USER, VIRTUOSO_PASS);
+        }catch(com.hp.hpl.jena.shared.JenaException e){
+            try {Thread.currentThread().sleep(10000);} catch (InterruptedException ex) {}
+            if(testNumber > 10)
+                throw e;
+            return getStore(graph,testNumber+1);
+        }
+    }
+    
     public static void addItems(ArrayList<Item> items){
         addItems(items.toArray(new Item[items.size()]));
     }
