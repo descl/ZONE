e081c8c3934b72fe99ab27e9cce0344746b957f7
Christophe Desclaux <descl@zouig.org>
2013-08-21 15:14:55 +0200

    remove some debug code

--- ZONE-extractor/ZONE-utils/src/main/java/org/zoneproject/extractor/utils/VirtuosoDatabase.java
+++ ZONE-extractor/ZONE-utils/src/main/java/org/zoneproject/extractor/utils/VirtuosoDatabase.java
@@ -274,8 +274,6 @@ public abstract class VirtuosoDatabase {
      */
     public static Item getOneItemByURI(String uri){
         String request = "SELECT ?relation ?value FROM <http://zone-project.org/datas/items> WHERE{  <"+uri+"> ?relation ?value}";
-        logger.info(uri);
-        logger.info(request);
         ResultSet results = runSPARQLRequest(request);
         return new Item(uri,results,uri,"relation","?value");
     }
