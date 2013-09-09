36cfba3dd83886291d8569772f5e7f61e1ed7144
Christophe Desclaux <descl@zouig.org>
2013-09-09 15:46:07 +0200

    fix bug in transmission of lang param between pages

--- ZONE-watcher/app/controllers/application_controller.rb
+++ ZONE-watcher/app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
     end
     I18n.locale = params[:locale] || lang || I18n.default_locale
   end
+  # app/controllers/application_controller.rb
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { locale: I18n.locale }
+  end
 end
