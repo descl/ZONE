16973edb43f3c24d7a3788cc3b2dcf6c0e2ea2f0
Christophe Desclaux <descl@zouig.org>
2013-10-15 17:26:37 +0200

    [#58669676] implement the infinite scroll. Remove the gon gem! better code now for each item's loading.

---
+++ ZONE-extractor/ZONE-plugin-Spotlight/src/main/java/org/zoneproject/extractor/plugin/spotlight/App.java
@@ -0,0 +1,60 @@
+package org.zoneproject.extractor.plugin.wikimeta;
+
+/*
+ * #%L
+ * ZONE-plugin-WikiMeta
+ * %%
+ * Copyright (C) 2012 ZONE-project
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import java.util.ArrayList;
+import org.zoneproject.extractor.utils.Database;
+import org.zoneproject.extractor.utils.Item;
+import org.zoneproject.extractor.utils.Prop;
+import org.zoneproject.extractor.utils.ZoneOntology;
+
+/**
+ *
+ * @author Desclaux Christophe <christophe@zouig.org>
+ */
+
+public class App 
+{
+    private static final org.apache.log4j.Logger  logger = org.apache.log4j.Logger.getLogger(App.class);
+    public static String PLUGIN_URI = ZoneOntology.PLUGIN_WIKIMETA;
+    public static int SIM_DOWNLOADS = 20;
+    
+    public App(){
+        String [] tmp = {};
+        App.main(tmp);
+    }
+    
+    public static void main(String[] args) {
+        Item[] items = null;
+        String [] deps = {ZoneOntology.PLUGIN_EXTRACT_ARTICLES_CONTENT};
+        WikiMetaRequestThread[] th = new WikiMetaRequestThread[SIM_DOWNLOADS];
+        do{
+            items = Database.getItemsNotAnotatedForPluginsWithDeps(PLUGIN_URI,deps,SIM_DOWNLOADS);
+            logger.info("WikiMeta has "+items.length+" items to annotate");
+            for(Item item : items){
+                ArrayList<Prop> content= WikiMetaRequest.getProperties(item.concat());
+                Database.addAnnotations(item.getUri(), content);
+                Database.addAnnotation(item.getUri(), new Prop(App.PLUGIN_URI,"true"));
+            }
+        }while(items.length > 0);
+    }
+}
---
+++ ZONE-extractor/ZONE-plugin-Spotlight/src/main/java/org/zoneproject/extractor/plugin/spotlight/SpotlightRequest.java
@@ -0,0 +1,110 @@
+/**
+ * requests for WikiMeta
+ */
+package org.zoneproject.extractor.plugin.wikimeta;
+
+/*
+ * #%L
+ * ZONE-plugin-WikiMeta
+ * %%
+ * Copyright (C) 2012 ZONE-project
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.zoneproject.extractor.utils.Config;
+import org.zoneproject.extractor.utils.Prop;
+
+/**
+ *
+ * @author Desclaux Christophe <christophe@zouig.org>
+ */
+public class WikiMetaRequest {
+    public static String EntitiesURI = "http://www.wikimeta.org/Entities#";
+    
+    /*public static ArrayList<Prop> getProperties(Item item){
+        return getProperties(item.concat());
+    }
+    
+    */
+    public static ArrayList<Prop> getProperties(String texte){
+        String f = WikiMetaRequest_API.getResult(Config.getVar("wikiMeta-key"), WikiMetaRequest_API.Format.JSON, texte);
+        return analyseWikiMetaResult(f);
+    }
+    
+    /**
+     * used in debug mode in order to parse a result previously download from WikiMeta
+     * @param file the output of WikiMeta
+     * @return the list of properties
+     */
+    public static ArrayList<Prop> getProperties(File file){
+        
+        String content = "";
+        try{
+            FileInputStream fstream = new FileInputStream(file.getPath());
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            while ((strLine = br.readLine()) != null)   {
+                content += "\n"+strLine;
+            }
+            in.close();
+        }catch (Exception e){
+            Logger.getLogger(WikiMetaRequest.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return analyseWikiMetaResult(content);
+    }
+    
+    /**
+     * 
+     * @param content the json result content
+     * @return the list of propertyes
+     */
+    private static ArrayList<Prop> analyseWikiMetaResult(String JSONcontent){
+        ArrayList<Prop> result = new ArrayList<Prop>();
+        
+        ArrayList<LinkedHashMap> namedEntities = WikiMetaRequest_API.getNamedEntities(JSONcontent);
+        for(int i=0; i< namedEntities.size();i++){
+            
+            LinkedHashMap cur = namedEntities.get(i);
+            if(cur.get("type").toString().equals("AMOUNT")|| cur.get("type") == "") {
+                continue;
+            }
+            if(cur.containsKey("LINKEDDATA")){
+                Prop p;
+                if(cur.get("LINKEDDATA").equals("") || cur.get("LINKEDDATA").equals("null")){
+                    p = new Prop(WikiMetaRequest.EntitiesURI+""+cur.get("type").toString(), cur.get("EN").toString(), true,true);
+                }
+                else {
+                    p = new Prop(WikiMetaRequest.EntitiesURI+""+cur.get("type").toString(), cur.get("LINKEDDATA").toString(), false,true);
+                }
+                if(!result.contains(p)) {
+                    result.add(p);
+                }
+            }
+        }
+        return result;
+    }
+    
+}
