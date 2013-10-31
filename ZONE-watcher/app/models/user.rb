ec6204cce3fe293626865020f000da2ba0d15bdf
Christophe Desclaux <descl@zouig.org>
2013-10-31 15:40:02 +0100

    fix bug from previous commit for

--- ZONE-watcher/app/models/user.rb
+++ ZONE-watcher/app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
 
   def self.find_for_provider_oauth(auth, signed_in_resource=nil)
     user = User.where('uid = ?', auth.uid).first
-    if user != nil && ((user.provider == nil && auth == "reador") || ((user.provider != nil) && (!user.provider.include? auth.provider))
+    if user != nil && ((user.provider == nil && auth == "reador") || ((user.provider != nil) && (!user.provider.include? auth.provider)))
       user = nil
     end
     unless user
