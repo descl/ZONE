f3284ca54a3073c09c6a94fcb8501cd8943a2753
Christophe Desclaux <descl@zouig.org>
2013-10-31 17:31:04 +0100

    fix bug from previous commit for

--- ZONE-watcher/config/environments/development.rb
+++ ZONE-watcher/config/environments/development.rb
@@ -1,3 +1,6 @@
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 ZONEWatcher::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
